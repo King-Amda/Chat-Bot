e26b4645f9eee0e7483c48195da76e4e26db6b4c
King Amda ••• ✪ <86409667+King-Amda@users.noreply.github.com>
2021-09-18 17:24:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,23 +14,23 @@ KINGAMDA = Client(
     api_hash = os.environ["API_HASH"]
 )
 
-START_IMG = "https://telegra.ph/file/66623ce5d08ec0323ce40.jpg"
+START_IMG = "https://telegra.ph/file/0ee669c1d3e0120424822.jpg"
 
 START_TEXT = """
-𝐃𝐚𝐫𝐤 𝐒𝐡𝐚𝐫𝐤 ගෙ 𝐏𝐌 𝐒𝐞𝐜𝐮𝐫𝐢𝐭𝐲 එකෙන් ඔයාට 𝐃𝐚𝐫𝐤 𝐒𝐡𝐚𝐫𝐤 ට 𝐌𝐬𝐠 දාන්න දෙන්නෙ නෑ ..
+𝐊𝐢𝐧𝐠 𝐀𝐦𝐝𝐚 ගෙ 𝐏𝐌 𝐒𝐞𝐜𝐮𝐫𝐢𝐭𝐲 එකෙන් ඔයාට 𝐊𝐢𝐧𝐠 𝐀𝐦𝐝𝐚 ට 𝐌𝐬𝐠 දාන්න දෙන්නෙ නෑ ..
 
 ඒකට විකල්පයක් විදියට තමයි 𝐊𝐢𝐧𝐠 𝐀𝐦𝐝𝐚 𝐁𝐨𝐭 ව හැදුවෙ..
 
-ඔයාලට මේකෙන් 𝐃𝐚𝐫𝐤 𝐒𝐡𝐚𝐫𝐤 ව සම්බන්ධ කරගන්න පුලුවන්.
+ඔයාලට මේකෙන් 𝐊𝐢𝐧𝐠 𝐀𝐦𝐝𝐚 ව සම්බන්ධ කරගන්න පුලුවන්.
 
 ගොඩ දෙනෙක් අහන ප්‍රශ්න යට බටන් වල තියෙනවා..
 ඔයාට ඕන ඒවා තියෙනම් එතනින් ගන්න පුලුවන්.. 
-යටම තීන 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐃𝐚𝐫𝐤 𝐒𝐡𝐚𝐫𝐤 බටන් එකෙන් ඔයාට 𝐃𝐚𝐫𝐤 𝐒𝐡𝐚𝐫𝐤 ව සම්බන්ධ කර ගන්න පුලුවන්.
+යටම තීන 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐊𝐢𝐧𝐠 𝐀𝐦𝐝𝐚 බටන් එකෙන් ඔයාට 𝐊𝐢𝐧𝐠 𝐀𝐦𝐝𝐚 ව සම්බන්ධ කර ගන්න පුලුවන්..
 """
 
 START_BUTTON = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('King Amda Telegram',url='https://t.me/......'),
+        InlineKeyboardButton('King Amda Telegram',url='https://t.me/KingAmda'),
         InlineKeyboardButton('King Amda Whatsap',url='https://wa.me/94783752040')
         ],
         [
